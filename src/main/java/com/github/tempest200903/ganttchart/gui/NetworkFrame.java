2513b618fe30c4a39604eccfe117d19bc14b04dc
tempest200903 <tempest200903@gmail.com>
2015-05-17 16:06:29 +0900

    #4 Fixed predecessor を変更したら TimelineChart を再描画する。シーケンス図を追加。

--- src/main/java/com/github/tempest200903/ganttchart/gui/NetworkFrame.java
+++ src/main/java/com/github/tempest200903/ganttchart/gui/NetworkFrame.java
@@ -2,5 +2,11 @@ package com.github.tempest200903.ganttchart.gui;
 
 import javax.swing.JInternalFrame;
 
-class NetworkFrame extends JInternalFrame   {
+class NetworkFrame extends JInternalFrame {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
 }
