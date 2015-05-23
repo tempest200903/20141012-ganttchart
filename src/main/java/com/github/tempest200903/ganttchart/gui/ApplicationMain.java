6a938b0279abbdec7de62188db7356364a111c9b
tempest200903 <tempest200903@gmail.com>
2015-05-23 21:38:24 +0900

    Fix #10 logging ライブラリを導入する

--- src/main/java/com/github/tempest200903/ganttchart/gui/ApplicationMain.java
+++ src/main/java/com/github/tempest200903/ganttchart/gui/ApplicationMain.java
@@ -1,5 +1,7 @@
 package com.github.tempest200903.ganttchart.gui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
@@ -12,14 +14,22 @@ import javax.swing.JFrame;
  */
 class ApplicationMain {
 
-	public static void main(String[] args) {
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		MainFrame mainFrame = new MainFrame("ProjectFrame");
-		mainFrame.setLocation(10, 10);
-		mainFrame.setSize((int) (screenSize.width * 0.6),
-				(int) (screenSize.height * 0.6));
-		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.setVisible(true);
-	}
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ApplicationMain.class);
+        logger.debug("Hello World debug");
+        logger.trace("Hello World trace");
+        logger.info("Hello World info");
+        logger.warn("Hello World warn");
+        logger.error("Hello World error");
+        System.exit(0);
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        MainFrame mainFrame = new MainFrame("ProjectFrame");
+        mainFrame.setLocation(10, 10);
+        mainFrame.setSize((int) (screenSize.width * 0.6),
+                (int) (screenSize.height * 0.6));
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+    }
 
 }
